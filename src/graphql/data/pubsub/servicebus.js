1495e29e16868765485e0271797609cf62a0cda4
rosteele <rosteele@starbucks.com>
2021-02-16 22:29:19 +0000

    remove logging

--- src/graphql/data/pubsub/servicebus.js
+++ src/graphql/data/pubsub/servicebus.js
@@ -1,6 +1,5 @@
 const { ServiceBusClient } = require('@azure/service-bus')
 const { env: { SB_QUEUE_CONN_STRING } } = process
-console.log(`SB_QUEUE_CONN_STRING ${SB_QUEUE_CONN_STRING}`)
 const client = new ServiceBusClient(SB_QUEUE_CONN_STRING)
 
 module.exports = client
