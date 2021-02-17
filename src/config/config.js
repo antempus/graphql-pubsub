0ff2b7246ecca6e5f70501a1eca8698cf937e90d
rosteele <rosteele@starbucks.com>
2021-02-17 02:15:37 +0000

    add config object

---
+++ src/config/config.js
@@ -0,0 +1,25 @@
+// List of required environment variables in local .env file
+const environmentVariables = [
+
+]
+
+// check that all environment variables are specified
+environmentVariables.forEach(environmentVariable => {
+  if (!process.env[environmentVariable]) {
+    throw new Error(`Required environment variable ${environmentVariable} is missing.`)
+  }
+})
+
+// generate config obj with all environment variables
+const config = {
+  REDIS: {
+    HOST: process.env.REDIS_HOST,
+    PORT: process.env.REDIS_PORT,
+    PASSWORD: process.env.PASSWORD
+  },
+  SERICE_BUS: {
+    CONN_STRING: process.env.SB_QUEUE_CONN_STRING
+  }
+}
+
+module.exports = config
